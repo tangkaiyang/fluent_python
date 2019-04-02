02b87334fe9e33d0681a74094e615aeddfd4426a
tangkaiyang <945541696@qq.com>
2019-04-02 08:20:48 +0800

    Unicode

--- chapter04/text_bytearray.py
+++ chapter04/text_bytearray.py
@@ -230,24 +230,24 @@ Python3内置的open函数在会在读取文件时做必要的解码,以文本�
 编码默认值:一团糟
 """
 # 示例4-11 探索编码默认值
-import sys, locale
-expressions = """
-    locale.getpreferredencoding()
-    type(my_file)
-    my_file.encoding
-    sys.stdout.isatty()
-    sys.stdout.encoding
-    sys.stdin.isatty()
-    sys.stdin.encoding
-    sys.stderr.isatty()
-    sys.stderr.encoding
-    sys.getdefaultencoding()
-    sys.getfilesystemencoding()
-"""
-my_file = open('dummy', 'w')
-for expression in expressions.split():
-    value = eval(expression)
-    print(expression.rjust(30), '->', repr(value))
+# import sys, locale
+# expressions = """
+#     locale.getpreferredencoding()
+#     type(my_file)
+#     my_file.encoding
+#     sys.stdout.isatty()
+#     sys.stdout.encoding
+#     sys.stdin.isatty()
+#     sys.stdin.encoding
+#     sys.stderr.isatty()
+#     sys.stderr.encoding
+#     sys.getdefaultencoding()
+#     sys.getfilesystemencoding()
+# """
+# my_file = open('dummy', 'w')
+# for expression in expressions.split():
+#     value = eval(expression)
+#     print(expression.rjust(30), '->', repr(value))
 # chcp输出当前控制台激活的代码页:
 # 把输出重定向到文件 python .py > encodings.log
 # locale.getpreferredencoding()返回的编码是最重要的:
@@ -282,5 +282,155 @@ NFC(Normalization Form C)使用最少的码位构成等价的字符串,而NFD把
 # print(normalize('NFD', s1) == normalize('NFD', s2))
 """
 用户输入的文本默认是NFC形式.不过安全起见,保存文本之前,最好使用normalize('NFC', user_text)清洗字符串
+"""
+# 使用NFC时,有些单字符会被规范成另一个单字符.
+# 例如电阻单位欧姆(Ω)会被规范成希腊字母大写的欧米加.
+# 这两个字符在视觉上是一样的,但是比较式并不相等,因此要规范化,防止出现意外
+# from unicodedata import normalize, name
+# ohm = '\u2126'
+# print(name(ohm))
+# ohm_c = normalize('NFC', ohm)
+# print(name(ohm_c))
+# print(ohm == ohm_c)
+# print(normalize('NFC', ohm) == normalize('NFC', ohm_c))
+"""
+在另外两个规范化形式(NFKC和NFKD)的首字母缩略词中,
+字母K表示compatibility(兼容性).
+这两种是比较严格的规范化形式,对"兼容字符"有影响.
+虽然Unicode的目标是为各个字符提供"规范"的码位,但是为了兼容现有的标准,
+有些字符会出现多次.
+例如,虽然希腊字母表中有"μ"这个字母(码位是U+03BC, GREEK SMALL LETTER MU),
+但是Unicode还是加入了微符号'μ'(U+00B5),以便于latin1相互转换.
+因此,微符号是一个"兼容字符".
+在NFKC和NFKD形式中,各个兼容字符会被替换成一个或多个"兼容分解"字符,
+即使这样有些格式损失,但仍是"首选"表述--理想情况下,格式化是外部标记的
+职责,不应由Unicode处理
+"""
+# 示例: 二分之一 '½'（U+00BD）经过兼容分解后得到的是三个字符序列
+# '1/2'；微符号 'µ'（U+00B5）经过兼容分解后得到的是小写字母
+# 'μ'（U+03BC）
+# 下面是NFKC的具体应用:
+# from unicodedata import normalize, name
+# half = '½'
+# print(normalize('NFKC', half))
+# four_squared = '4²'
+# print(normalize('NFKC', four_squared))
+# micro = 'μ'
+# micro_kc = normalize('NFKC', micro)
+# print(micro, micro_kc)
+# print(ord(micro), ord(micro_kc))
+# print(name(micro), name(micro_kc))
+"""
+使用NFKC和NFKD规范化形式时要小心,而且只能在特殊情况中使用,例如搜索和索引,
+而不能用于持久存储,因为这两种转换会导致数据损失.
 
+4.6.1 大小写折叠
+大小写折叠其实就是把所有文本编程小写,再做些其他转换.
+这个功能由str.casefold()方法支持(Python3.3新增)
+对于只包含latin1字符的字符串s,s.casefold()得到的结果与s.lower()
+一样,唯有两个例外:微符号 'µ' 会变成小写的希腊字
+母“μ”（在多数字体中二者看起来一样）；德语 Eszett（“sharp s”，ß）
+会变成“ss”
+"""
+# from unicodedata import normalize, name
+# micro = 'μ'
+# print(name(micro))
+# micro_cf = micro.casefold()
+# print(name(micro_cf))
+# print(micro, micro_cf)
+# eszett = 'ß'
+# print(name(eszett))
+# eszett_cf = eszett.casefold()
+# print(eszett, eszett_cf)
+"""
+自Python3.4起,str.casefold()和str.lower()得到不同结果的有116个码位.
+Unicode6.3命名了110122个字符,这只占了0.11%
+与Unicode相关的任何问题一样,大小写折叠是个复杂的问题,有很多语言上的特殊情况,但是Python核心团队提供了一种方法,能满足大多数用户的需求
+
+4.6.2 规范化文本匹配实用函数
+由前文可知,NFC和NFD可以放心使用,而且能合理比较Unicode字符串.
+对大多数应用来说,NFC是最好的规范化形式.
+不区分大小写的比较应该使用str.casefold()
+如果要处理多语言文本,工具箱中应该有示例4-13中的nfc_equal和fold_equal函数
+"""
+# 实例4-13 normeq.py: 比较规范化Unicode字符串
+"""
+4.6.3 极端"规范化": 去掉变音符号
+"""
+# 示例 4-14 去掉全部组合记号的函数(在sanitize.py模块中)
+# import unicodedata
+# import string
+# def shave_marks(txt):
+#     """去掉全部变音符号"""
+#     norm_txt = unicodedata.normalize('NFD', txt) # 把所有字符分解成基字符和组合记号
+#     shaved = ''.join(c for c in norm_txt
+#                      if not unicodedata.combining(c)) # 过滤掉所有组合记号
+#     return unicodedata.normalize('NFC', shaved) # 重组所有字符
+# order = '“Herr Voß: • ½ cup of OEtker™ caffè latte • bowl of açaí.”'
+# print(shave_marks(order))
+# Greek = 'Zέφupoς, Zéfiro'
+# print(shave_marks(Greek))
+"""
+去掉变音符号是为了把拉丁文本变成纯粹的ASCII,
+但是shave_marks函数还会修改非拉丁字符(如希腊字母),
+而只去掉重音符并不能把他们变成ASCII字符.
+因此,我们应该分析各个基字符,仅当字符在拉丁字母表中时才删除附加记号
+"""
+# 实例4-16 删除拉丁字母中组合记号的函数
+# from unicodedata import normalize, combining
+# import string
+# def shave_marks_latin(txt):
+#     """把拉丁基字符中所有的变音符号删除"""
+#     norm_txt = normalize('NFD', txt)
+#     latin_base = False
+#     keepers = []
+#     for c in norm_txt:
+#         if combining(c) and latin_base:
+#             continue    # 忽略拉丁基字符上的变音符号
+#         keepers.append(c)
+#         # 如果不是组合字符,那就是新的基字符
+#         if not combining(c):
+#             latin_base = c in string.ascii_letters
+#     shaved = ''.join(keepers)
+#     return normalize('NFC', shaved)
+# 4.7 Unicode文本排序
+# 4.8 Unicode数据库
+"""
+4.9 支持字符串和字节序列的双模式API
+标准库中的一些函数能接受字符串或字节序列为参数,然后根据类型展现不同的行为.
+re和os模块就有这样的函数
+4.9.1 正则表达式中的字符串和字节序列
+如果使用字节序列构建正则表达式,\d(匹配数字)和\w(匹配字母或数字或下划线或汉字)等模式就只能匹配ASCII字符;
+相比之下,如果是字符串模式,就能匹配ASCII之外的Unicode数字或字母
+"""
+# 实例4-22 ramanujan.py: 比较简单的字符串正则表达式和字节序列正则表达式的行为
+# import re
+# re_numbers_str = re.compile(r'\d+')
+# re_words_str = re.compile(r'\w+')
+# re_numbers_bytes = re.compile(rb'\d+')
+# re_words_bytes = re.compile(rb'\w+')
+# text_str = ("Ramanujan saw \u0be7\u0bed\u0be8\u0bef"
+#             " as 1729 = 1³ + 12³ = 9³ + 10³.")
+# text_bytes = text_str.encode('utf_8')
+# print('Text', repr(text_str), sep='\n   ')
+# print('Numbers')
+# print(' str :', re_numbers_str.findall(text_str))
+# print(' bytes:', re_numbers_bytes.findall(text_bytes))
+# print('Words')
+# print(' str :', re_words_str.findall(text_str))
+# print(' bytes:', re_words_bytes.findall(text_bytes))
+"""
+字符串正则表达式有个re.ASCII标志,
+它让\w,\W,\b,\B,\d,\D,\s和\S只匹配ASCII字符
+
+4.9.2 os函数中的字符串和字节序列
+GNU/Linux内核不理解Unicode,因此,对任何合理的编码方案来说,
+在文件名中使用字节序类都是无效的,无法解码成字符串.
+在不同操作系统中使用各种客户端的文件服务器,在遇到这个问题时尤其容易出错.
+为了规避这个问题,os模块中的所有函数,文件名或者路径名参数既能使用字符串,也能使用字节序列.
+如果这样的函数使用字符串参数低啊用,该参数会使用sys.getfilesystemencoding()得到的编解码器
+自动编码,然后操作系统会使用相同的编解码器解码.
+但是,如果必须处理(也可能是修正)那些无法使用上述方式自动处理的文件名,可以把字节序列参数传给
+os模块中的函数,得到字节序列返回值.
+这一特性允许我们处理任何文件名或路径名,不管里面有多少鬼符
 """
